23e4520db4a67b861e3159c1aaacf35b95ed2419
DanielleMathey95 <danielle.mathey@yahoo.com>
2020-02-23 18:49:41 -0800

    finished universities section

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -70,8 +70,10 @@ const graduates = [
 /* Request 1: Create a new array called universities that contains all the universities in the graduates array. This will be an array of strings.
 
 Once you have the new array created, sort the universities alphabetically and log the result. */
-const universities = [];
-console.log(universities);
+const universities = graduates.map(function (grad) {
+  return grad.university;
+});
+console.log(universities.sort());
 
 /* Request 2: Create a new array called contactInfo that contains both first name and email of each student. This will be an array of strings.
 
@@ -79,14 +81,20 @@ The resulting contact information strings should have a space between the first 
 "Josh josh@example.com"
 
 Log the result of your new array. */
-const contactInfo = [];
+const contactInfo = graduates.map(function (grad) {
+  return `${grad.first_name} ${grad.email}`
+});
 console.log(contactInfo);
 
 /* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called unisWithUni that contains them all. This will be an array of objects. Log the result. */
-const unisWithUni = [];
-console.log(unisWithUni);
+const uni = [];
 
-
+for (i = 0; i < graduates.length; i++) {
+  graduates[i].university;
+  if (university === uni)
+  uni.push(graduates[i].university);
+}
+console.log(uni);
 // ==== ADVANCED Array Methods ====
 
 // Given this zoo data from around the United States, follow the instructions below.  Use the specific array methods in the requests below to solve the problems.
